6bfff069e85fc46cc61f3f408dd49291088c4c1e
Nael <nael.fradet@gmail.com>
2023-10-10 19:13:44 +0200

    fix itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: nfradet <nfradet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 15:13:11 by nfradet           #+#    #+#             */
-/*   Updated: 2023/10/10 18:53:02 by nfradet          ###   ########.fr       */
+/*   Updated: 2023/10/10 19:12:58 by nfradet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ static int	ft_intlen(unsigned int n)
 	return (count);
 }
 
-//#include <stdio.h>
 char	*ft_itoa(int n)
 {
 	int		sign;
@@ -41,11 +40,12 @@ char	*ft_itoa(int n)
 		sign = 1;
 	}
 	len = ft_intlen((unsigned int)n) + 1 + sign;
-	//printf("%d\n", len);
 	str = malloc(sizeof(char) * (len));
 	if (str == NULL)
 		return (NULL);
 	str[len - 1] = '\0';
+	if (n == 0)
+		str[len - len] = '0';
 	while (--len >= sign && (unsigned int)n >= 1)
 	{
 		str[len - 1] = ((unsigned int)n % 10) + 48;
@@ -53,15 +53,13 @@ char	*ft_itoa(int n)
 	}
 	if (sign == 1)
 		str[len - 1] = '-';
-	else if (n == 0)
-		str[len - 1] = '0';
 	return (str);
 }
 
 /*
 int	main()
 {
-	char  *str = ft_itoa(1000034);
+	char  *str = ft_itoa(9);
 	printf("%s\n", str);
 	free(str);
 }*/
